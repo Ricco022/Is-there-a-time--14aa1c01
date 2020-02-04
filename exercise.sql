5dc83406d4e967d4a5fa41a4c425be32e7b1a7e5
Riccardo van der Horst <riccardo271002@gmail.com>
2020-02-04 14:57:34 +0100

    Uitwerking van de exercise

---
+++ exercise.sql
@@ -0,0 +1,7 @@
+alter table planeten modify naam varchar(20) not null;
+
+alter table planeten modify diameter int not null;
+
+alter table planeten modify `afstand tot de zon` int not null;
+
+alter table planeten modify massa int not null;
